e0ff00c6b796d1a9bade15019a4d0508ea52f172
Eric Frees <efrees@atlasrfid.com>
2021-04-09 21:14:10 -0500

    String-based AppendSort (2021 1A)

--- codeJam2021/Round1A/AppendSort.cs
+++ codeJam2021/Round1A/AppendSort.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Round1A
@@ -13,82 +14,112 @@ namespace Round1A
             while (k <= T)
             {
                 var N = int.Parse(Console.ReadLine());
-                var array = Console.ReadLine().Split().Select(long.Parse).ToArray();
+                var array = Console.ReadLine().Split().ToArray();
 
                 var answer = 0;
-                var previous = 0L;
+                var previous = "0";
 
                 foreach (var item in array)
                 {
-                    if (previous < item)
+                    var firstDifferingDigits = GetFirstDifferingDigits(previous, item);
+
+                    var previousIsLessThanCurrent = previous.Length < item.Length
+                                                    || (previous.Length == item.Length && firstDifferingDigits != null &&
+                                                        firstDifferingDigits.PreviousIsLess());
+                    if (previousIsLessThanCurrent)
                     {
                         // already done
                         previous = item;
+                    Debug.WriteLine(previous);
                         continue;
                     }
 
-                    var previousAsString = previous.ToString();
-                    var itemAsString = item.ToString();
+                    // Now, previous is known to be longer or equal length to item
 
-                    var previousPrefix = previousAsString.Substring(0, itemAsString.Length);
-                    var previousPostfix = previousAsString.Substring(itemAsString.Length); //can be empty
-                    var prefixValue = long.Parse(previousPrefix);
-                    if (prefixValue > item
-                        || (prefixValue == item && previousPostfix.All(c => c == '9')))
+                    var previousPrefix = previous.Substring(0, item.Length);
+                    var previousPostfix = previous.Substring(item.Length); //can be empty
+
+                    var firstDifferingInPrefix = GetFirstDifferingDigits(previousPrefix, item);
+                    var previousPrefixIsLarger =  firstDifferingInPrefix != null && firstDifferingInPrefix.PreviousIsGreater();
+
+                    if (previousPrefixIsLarger
+                        || (previousPrefix == item && previousPostfix.All(c => c == '9')))
                     {
                         //We have to make it longer, so just go with the smallest longer number, adding zeros
-                        var digitsToAdd = previousAsString.Length + 1 - itemAsString.Length;
+                        var digitsToAdd = previous.Length + 1 - item.Length;
                         answer += digitsToAdd;
 
-                        previous = item;
-                        while (digitsToAdd > 0)
-                        {
-                            digitsToAdd--;
-                            previous *= 10;
-                        }
+                        previous = item + new string('0', digitsToAdd);
                     }
                     else
                     {
                         // We can make it larger at the same length, if remaining digits aren't 9's or if item is already greater than prefix.
-                        var digitsToAdd = previousAsString.Length - itemAsString.Length;
+                        var digitsToAdd = previous.Length - item.Length;
                         answer += digitsToAdd;
 
-                        if (prefixValue == item)
+                        if (previousPrefix == item)
                         {
                             // we have to increment the postfix
+                            // 10 | 00 => 10 | 01
+                            // 10 | 009 => 10 | 010
+                            // 10 | 209 => 10 | 210
                             var incrementedPostfix = previousPostfix.ToCharArray();
                             var incremented = false;
-                            for (var i = 0; i < incrementedPostfix.Length; i++)
+                            for (var i =  incrementedPostfix.Length -1; i >= 0; i--)
                             {
-                                if (!incremented && incrementedPostfix[i] != '9')
+                                if (incremented)
+                                {
+                                    continue;
+                                }
+
+                                if (incrementedPostfix[i] == '9')
+                                {
+                                    incrementedPostfix[i] = '0';
+                                }
+                                else
                                 {
                                     incrementedPostfix[i] = (char)(incrementedPostfix[i] + 1);
                                     incremented = true;
                                 }
-                                else if (incremented)
-                                {
-                                    incrementedPostfix[i] = '0';
-                                }
                             }
 
-                            var newValueAsString = previousPrefix + incrementedPostfix;
-                            previous = long.Parse(newValueAsString);
+                            var newValueAsString = previousPrefix + new string(incrementedPostfix);
+                            previous = newValueAsString;
                         }
                         else
                         {
                             // we can just fill with zeros
-                            previous = item;
-                            while (digitsToAdd > 0)
-                            {
-                                digitsToAdd--;
-                                previous *= 10;
-                            }
+                            previous = item + new string('0', digitsToAdd);
                         }
                     }
+                    Debug.WriteLine(previous);
                 }
 
                 Console.WriteLine($"Case #{k}: {answer}");
                 k++;
+            }
+        }
+
+        private static CharPair GetFirstDifferingDigits(string previous, string item)
+        {
+            return previous
+                .Zip(item, (cp, ci) => new CharPair { prev = cp, cur = ci })
+                .FirstOrDefault(x => x.prev != x.cur);
+        }
+
+        class CharPair
+        {
+            public char prev;
+            public char cur;
+
+            public bool PreviousIsLess()
+            {
+                return prev < cur;
+            }
+
+            public bool PreviousIsGreater()
+            {
+                return prev > cur;
             }
         }
     }
